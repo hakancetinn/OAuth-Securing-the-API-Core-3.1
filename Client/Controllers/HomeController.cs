6a60335061b7758e8162467132cd755073e11a6a
Hakan CETIN <hakan_cetin1205@hotmail.com>
2020-07-09 23:28:30 +0300

    first commit

---
+++ Client/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Controllers
+{
+    public class HomeController:Controller
+    {
+        private readonly HttpClient _client;
+
+        public HomeController(IHttpClientFactory httpClientFactory)
+        {
+            _client = httpClientFactory.CreateClient();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Secret()
+        {
+            var token = await HttpContext.GetTokenAsync("access_token");
+
+            _client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+
+            var serverResponse = await _client.GetAsync("http://localhost:55740/secret/index");
+
+            var apiResponse = await _client.GetAsync("http://localhost:63520/secret/index");
+
+            return View();
+        }
+    }
+}
+
---
+++ Server/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public static class Constants
+    {
+        public const string Issuer = Audiance;
+        public const string Audiance = "http://localhost:55740/";
+        public const string Secret = "not_too_short_secret_otherwise_it_might_error";
+    }
+}
